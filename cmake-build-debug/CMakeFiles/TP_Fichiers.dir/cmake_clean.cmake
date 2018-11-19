d62acba0fc5e2f92b928f31bd62992abb820dff9
Mouad <hm_benmoussat@esi.dz>
2018-11-19 17:02:11 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TP_Fichiers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_Fichiers.dir/main.c.obj"
+  "TP_Fichiers.pdb"
+  "TP_Fichiers.exe"
+  "TP_Fichiers.exe.manifest"
+  "libTP_Fichiers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TP_Fichiers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
