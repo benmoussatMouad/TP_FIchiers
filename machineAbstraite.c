253fcbb87ae929a80686b511efc45324e4b8f90f
Mouad <hm_benmoussat@esi.dz>
2018-11-24 21:18:50 +0100

    Machine Abstraite Implementé Un ou deux erreurs au niveaux de LireDir et EcrireDir.

---
+++ machineAbstraite.c
@@ -0,0 +1,90 @@
+#include "machineAbstraite.h"
+
+//**************** IMPLEMENTATION DES MODELES **************
+
+int Ouvrir (FICHIER f, char* nomFichier, char mode) {
+    //Retourne 0 si il n'y a pas d'erreur
+    // Retourne une valeur diff de 0 sinon
+
+    if ( mode == 'a' ) {
+        f.file = fopen(nomFichier, "r+");
+        f.entete.carInseres = 0;
+        f.entete.carSupprimes = 0;
+        f.entete.nbArticles = 0;
+        f.entete.nbBlocs = 0;
+        f.blocCourrent = 0;
+        return 0;
+    }
+    else if( mode == 'n') {
+        f.file = fopen(nomFichier, "w");
+        f.entete.carInseres = 0;
+        f.entete.carSupprimes = 0;
+        f.entete.nbArticles = 0;
+        f.entete.nbBlocs = 0;
+        f.blocCourrent = 0;
+        return 0;
+    }
+    else
+        return 1;
+}
+
+void Fermer (FICHIER f) {
+    fclose(f.file);
+}
+
+void LireDir (FICHIER f, int i, BLOC buf) {
+    fseek(f.file, i* sizeof(BLOC), SEEK_SET);
+    fread(&buf,TAILLE_BLOC, 1, f.file);
+}
+
+void EcrireDir (FICHIER f, int i, BLOC buf) {
+    fseek(f.file, i * sizeof(BLOC), SEEK_SET);
+    fwrite(&buf, TAILLE_BLOC, 1, f.file);
+}
+
+int Entete (FICHIER f, int i) {
+    switch (i) {
+        case 0:
+            return f.entete.nbBlocs;
+        case 1:
+            return f.entete.nbArticles;
+        case 2:
+            return f.entete.carSupprimes;
+        case 3:
+            return f.entete.carInseres;
+        default:
+            return -1;
+    }
+}
+
+void LireSeq (FICHIER f, BLOC buf) {
+    LireDir(f, f.blocCourrent, buf);
+    f.blocCourrent++;
+}
+
+void EcrireSeq (FICHIER f, BLOC buf) {
+    EcrireDir(f, f.blocCourrent, buf);
+    f.blocCourrent++;
+}
+
+void Aff_Entete (FICHIER f, int i, int val) {
+    switch (i) {
+        case 0:
+            f.entete.nbBlocs = val;
+            break;
+        case 1:
+            f.entete.nbArticles = val;
+            break;
+        case 2:
+            f.entete.carInseres = val;
+            break;
+        case 3:
+            f.entete.carSupprimes = val;
+            break;
+        default:
+            break;
+    }
+}
+
+
+
--- machineAbstraite.h
+++ machineAbstraite.h
@@ -1,35 +1,51 @@
 
 #ifndef TP_FICHIERS_MACHINEABSTRAITE_H
 #define TP_FICHIERS_MACHINEABSTRAITE_H
+//***********
+#include "stdio.h"
+#include "string.h"
+
+#define TAILLE_BLOC 999;
 
 //***********Declaration des types**
 
 typedef struct Bloc {
-    char* article;
+    char* info;
     char efface;
     char cle[4];
-    int chevauch; //indique la taille du chevauchement de l'article precedent dans le bloc
+    char chevauch;//indique la taille du chevauchement de l'article precedent dans le bloc
     //si egal à 0 alors pas de chevauchement
 } BLOC;
 
 typedef struct fichier {
-    int nbBlocs;
-    int nbArticles;
-    int carSupprimes;
-    int carInseres;
-    BLOC* blocs;
+    struct Tentete {
+        int nbBlocs;
+        int nbArticles;
+        int carSupprimes;
+        int carInseres;
+    } entete;
+    int blocCourrent;
+    FILE* file;
 } FICHIER;
 
 
 //***********Procedures du Modele*******
 
-void Ouvrir(FICHIER f, char* nomFicheir, char mode);
+int Ouvrir(FICHIER f, char* nomFicheir, char mode);
+///----------------------------------------------
 void Fermer(FICHIER f);
+//-------------------------------------------------
 void LireDir(FICHIER f, int i, BLOC buf);
+//-------------------------------------------------
 void EcrireDir(FICHIER f, int i, BLOC buf);
-void LireSeq(FICHIER f, BLOC bud);
+//-------------------------------------------------
+int Entete(FICHIER f, int i);
+//-------------------------------------------------
+void LireSeq(FICHIER f, BLOC buf);
+//-------------------------------------------------
 void EcrireSeq(FICHIER f, BLOC buf);
-BLOC* AllocBloc(FICHIER f);
+//-------------------------------------------------
+void Aff_Entete (FICHIER, int, int);
 
 
 #endif //TP_FICHIERS_MACHINEABSTRAITE_H
